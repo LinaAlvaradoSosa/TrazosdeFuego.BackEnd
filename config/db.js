1bc0e5704fff4aca51d82f2a27512030e427415a
linaA <hellolinita@gmail.com>
2025-11-29 15:09:45 -0500

    Usar MONGO_URI como variable de conexion

--- config/db.js
+++ config/db.js
@@ -3,11 +3,19 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 export async function connectDB() {
-    try {
-        await mongoose.connect(process.env.LINKDB)
-        console.log('conectado a la base de datos');
-        
-    } catch (error) {
-        console.log(`no se pudo conectar a la base de datos error: ${error.message}`);
+  try {
+    const uri = process.env.MONGO_URI; // 👈 cambiamos el nombre
+
+    if (!uri) {
+      console.error("❌ MONGO_URI no está definido");
+      return;
     }
+
+    await mongoose.connect(uri);
+    console.log("✅ Conectado a la base de datos");
+  } catch (error) {
+    console.log(
+      `no se pudo conectar a la base de datos error: ${error.message}`
+    );
+  }
 }
